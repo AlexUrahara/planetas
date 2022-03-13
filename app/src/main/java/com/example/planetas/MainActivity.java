7a7546f95ffa7d8c59ee461317ea438ed86de821
alexhurahara <fernayxxx@gmail.com>
2022-03-13 01:03:44 -0600

    con TOAST

--- app/src/main/java/com/example/planetas/MainActivity.java
+++ app/src/main/java/com/example/planetas/MainActivity.java
@@ -1,44 +1,34 @@
 package com.example.planetas;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
-
-
 public class MainActivity extends AppCompatActivity {
-
     ListView ListViewmenu_planetas;
     List<menu_planetas> lst;
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         ListViewmenu_planetas = findViewById(R.id.listaplanetas);
-
         AdaptarPlanets adapter = new AdaptarPlanets(this, GetData());
         ListViewmenu_planetas.setAdapter(adapter);
         ListViewmenu_planetas.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 menu_planetas c = lst.get(i);
-
+                Toast.makeText(getBaseContext(),c.nombre,Toast.LENGTH_SHORT).show();
             }
         });
-
     }
-
     private List<menu_planetas> GetData() {
         lst=new ArrayList<>();
-
         lst.add(new menu_planetas(1,R.drawable.mercurio,"Mercurio","el primer planeta"));
         lst.add(new menu_planetas(2,R.drawable.venus,"venus","el segundo planeta"));
         lst.add(new menu_planetas(3,R.drawable.tierra,"tierra","el tercer planeta"));
@@ -47,8 +37,6 @@ public class MainActivity extends AppCompatActivity {
         lst.add(new menu_planetas(6,R.drawable.saturno,"saturno","el sexto planeta"));
         lst.add(new menu_planetas(7,R.drawable.urano,"urano","el septimo planeta"));
         lst.add(new menu_planetas(8,R.drawable.neptuno,"neptuno","el octavo planeta"));
-
         return lst;
     }
-
 }
